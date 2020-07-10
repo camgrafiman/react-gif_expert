bc0dce49c89b5b0a19e9b6528c770af41ccf2fe0
Gallego <ManuelAlejandro.Gallego@wearematterkind.com>
2020-07-10 19:04:53 +0200

    docs agregado

---
+++ docs/precache-manifest.c64a4ffd4ece18ee613252eabb92bc83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "574eadcc470bd43a17b473a5252bed6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9f056c273e05dcde507",
+    "url": "/static/css/main.d1b061a1.chunk.css"
+  },
+  {
+    "revision": "660d15ea480792d416e1",
+    "url": "/static/js/2.3bed9c27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3bed9c27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9f056c273e05dcde507",
+    "url": "/static/js/main.8ad86bea.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  },
+  {
+    "revision": "18848c187cba29f2c66382165f436a13",
+    "url": "/static/media/loading.18848c18.gif"
+  }
+]);
