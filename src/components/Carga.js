2d1aabab91432f1c0f29bfc403c5e8fee5c5760f
Camgrafiman <camgrafiman@gmail.com>
2020-07-11 16:48:46 +0200

    Agregado GIF carga

--- src/components/Carga.js
+++ src/components/Carga.js
@@ -4,7 +4,7 @@ import loader from '../images/loading.gif';
 const Carga = () => {
     return (
         <div>
-            <img className='loading' src={loader} alt='Cargando...'></img>
+            <img className='loading' src='https://camgrafiman.github.io/react-gif_expert/static/media/loading.18848c18.gif' alt='Cargando...'></img>
         </div>
     );
 }
